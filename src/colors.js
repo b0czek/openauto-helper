686c480f48d958b2a543425431190d0082f898dc
Dariusz Majnert <dariusz.majnert@gmail.com>
2021-04-06 23:44:06 +0200

    switched to iniparser because it parses values starting with hash properly

--- src/colors.js
+++ src/colors.js
@@ -1,14 +1,13 @@
-const ini = require('ini');
-const fs = require('fs');
+const ini = require('iniparser');
 
 const readColorConfig = () => {
-    fs.readFile('/home/pi/.openauto/config/openauto_system.ini', (err, data) => {
+    ini.parse('/home/pi/.openauto/config/openauto_system.ini', (err, data) => {
         if (err) {
             console.log(err);
             return;
         }
-        let config = ini.parse(data.toString('utf-8'));
-        console.log(config);
+
+        console.log(data);
     });
 };
 
