fa056ba5d738363b283daa33f08569e0523295d1
Dariusz Majnert <dariusz.majnert@gmail.com>
2021-05-02 23:12:08 +0200

    enable content tile to passthrough classnames

--- src/components/ContentTiles.jsx
+++ src/components/ContentTiles.jsx
@@ -1,8 +1,13 @@
 import "./ContentTiles.scss";
 export const ContentTile = (props) => (
-    <div className="contentTile">{props.children}</div>
+    <div
+        className={`contentTile contentTileLong ${
+            props.className ? props.className : ""
+        }`}>
+        {props.children}
+    </div>
 );
 
 export const ContentTileShort = (props) => (
-    <div className="contentTileShort">{props.children}</div>
+    <div className="contentTile contentTileShort">{props.children}</div>
 );
