937dcabe1cbafad2061e820d0a618cf81a485b66
Dariusz Majnert <dariusz.majnert@gmail.com>
2021-05-02 23:12:21 +0200

    add date to watch

--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -1,30 +1,46 @@
 import { ContentTile } from "./ContentTiles";
 import "./Watch.scss";
 import React, { useEffect, useState } from "react";
-const Watch = () => {
-    const [time, setTime] = useState();
 
-    useEffect(() => {
-        // set the time when component is rendered
-        setTime(getTimeString());
-        // and update it every one second
-        setInterval(() => {
-            setTime(getTimeString());
-        }, 1000);
-    }, []);
-    return (
-        <ContentTile>
-            <div className="time unselectable">{time}</div>
-        </ContentTile>
-    );
-};
+class Watch extends React.Component {
+    state = {
+        time: "00:00",
+        date: "00 / 00 / 0000",
+    };
+
+    updateWatch = () => {
+        this.setState({
+            time: getTimeString(),
+            date: getDateString(),
+        });
+        setTimeout(this.updateWatch, 1000);
+    };
+
+    componentDidMount() {
+        this.updateWatch();
+    }
+    render() {
+        return (
+            <ContentTile className="unselectable watch">
+                <div className="time">{this.state.time}</div>
+                <div className="date">{this.state.date}</div>
+            </ContentTile>
+        );
+    }
+}
 
 const getTimeString = () => {
     let now = new Date();
-    return `${now
-        .getHours()
-        .toString()
-        .padStart(2, "0")}:${now.getMinutes().toString().padStart(2, "0")}`;
+    return `${leftPad(now.getHours(), 2)}:${leftPad(now.getMinutes(), 2)}`;
 };
+
+const getDateString = () => {
+    let now = new Date();
+    return `${leftPad(now.getDate(), 2)} 
+    / ${leftPad(now.getMonth() + 1, 2)} 
+    / ${now.getFullYear()}`;
+};
+
+const leftPad = (number, length) => number.toString().padStart(length, "0");
 
 export default Watch;
