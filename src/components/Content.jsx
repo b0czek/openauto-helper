e2bab3d6986d1fae9e4aedfb1ff790e3b7cc0bce
Dariusz Majnert <dariusz.majnert@gmail.com>
2021-05-23 14:08:15 +0200

    merge van icon into imgs dir

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,13 @@
 import OilPressureGauge from "./OilPressureGauge";
 import Watch from "./Watch";
 import Thermometer from "./Thermometer";
+import vanIcon from '../imgs/van.png';
 import "./Content.scss";
 const Content = () => (
     <div className="content">
         <OilPressureGauge ioName="oilpressure.0" />
         <Watch />
-        <Thermometer ioName="ds18b20.0" iconPath="/icons/van.png" />
+        <Thermometer ioName="ds18b20.0" iconPath={vanIcon} />
     </div>
 );
 export default Content;
