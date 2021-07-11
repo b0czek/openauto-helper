3437a56f096400bb1089d17fff7eb06fefad30d2
Dariusz Majnert <dariusz.majnert@gmail.com>
2021-07-11 16:32:42 +0200

    add reassuring on drastic change

--- electron/io/lightsensor.ts
+++ electron/io/lightsensor.ts
@@ -1,20 +1,24 @@
 import IOComponent, { IOComponentConfig } from "./ioComponent";
 import { RendererIO } from "./io";
-import Tsl2561, { Gains, IntegrationTimes } from "ada-tsl2561-ts";
+import Tsl2561 from "ada-tsl2561-ts";
 
 export interface LightSensorConfig extends IOComponentConfig {
     busNumber: number;
     address: number;
     readingInterval: number;
     changeInsensitivity: number;
-    gain?: Gains;
-    intergrationTime?: IntegrationTimes;
+    radicalReassuranceReadings: number;
+    radicalThreshold: number;
 }
+
+// 1/10th of total possible values
+const radicalChangeThreshold = Math.round(Math.pow(2, 16) / 10);
 
 export default class LightSensor extends IOComponent {
     private config: LightSensorConfig;
     private sensor: Tsl2561;
     private value: number | null = 0;
+    private reassuranceReadings: number = 0;
     private interval: NodeJS.Timeout;
 
     constructor(config: LightSensorConfig, ios: RendererIO) {
@@ -36,7 +40,8 @@ export default class LightSensor extends IOComponent {
                     this.config.address
                 );
                 await this.sensor.enable();
-
+                await this.sensor.setGain(0); //1x
+                await this.sensor.setIntegrationTime(2); //402ms
                 await this._readSensor();
                 this.interval = setInterval(
                     this._readSensor,
@@ -52,27 +57,43 @@ export default class LightSensor extends IOComponent {
     private _readSensor = async () => {
         let reading: number;
         try {
-            reading = await this.sensor.getLux();
-            if (isNaN(reading) || reading === null) {
-                // if the reading is invalid, act like it never happened
-                return;
-            }
-            reading = Math.round(reading);
-            // if there was an error last time reading sensor
-            if (this.value === null) {
-                this.value = reading;
-            } else if (
-                this.value + this.config.changeInsensitivity < reading ||
-                this.value - this.config.changeInsensitivity > reading
-            ) {
-                this.value = reading;
-                this.sendState(null, reading);
-                console.log(reading);
-            }
+            reading = await this.sensor.getBroadband();
+            console.log(reading);
         } catch (err) {
-            console.log(err);
             this.sendState(err);
             this.value = null;
+            return;
+        }
+        reading = Math.round(reading);
+
+        // if there was an error last time reading sensor
+        if (this.value === null) {
+            this.value = reading;
+            return;
+        }
+        // if change is too drastic
+        if (isOutsideRange(this.value, reading, radicalChangeThreshold)) {
+            // and there wasnt enough reassurance reading
+            if (
+                this.reassuranceReadings !==
+                this.config.radicalReassuranceReadings
+            ) {
+                // then add one and return
+                this.reassuranceReadings++;
+                return;
+            }
+        }
+        // if the change wasnt drastic or
+        // if there was enough reassurence readings, then reset its count
+        this.reassuranceReadings = 0;
+
+        // if the change is big enough to emit it
+        if (
+            isOutsideRange(this.value, reading, this.config.changeInsensitivity)
+        ) {
+            this.value = reading;
+            this.sendState(null, reading);
+            console.log(reading);
         }
     };
 
@@ -81,3 +102,5 @@ export default class LightSensor extends IOComponent {
         this.sensor.free();
     }
 }
+const isOutsideRange = (oldValue: number, newValue: number, range: number) =>
+    oldValue + range < newValue || oldValue - range > newValue;
